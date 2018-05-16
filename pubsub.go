9b6d676281bdc1170b6dd7e05c30229b40585b05
Camilo Zapata <ccamilozt@gmail.com>
2018-05-16 08:30:32 -0500

    first commit

---
+++ pubsub.go
@@ -0,0 +1,73 @@
+/*
+Package pubsub contains utility functions for working with local broker mqtt.
+*/
+package pubsub
+
+import (
+	"log"
+	"time"
+	"errors"
+	"fmt"
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+
+//this type store the status of connection
+type PubSub struct {
+	Conn	MQTT.Client
+	Err	chan error
+}
+
+var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+  fmt.Printf("TOPIC: %s\n", msg.Topic())
+  fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
+//NewConnection return the PubSub object. nameClient is the client name in local broker.
+func NewConnection(nameClient string) (*PubSub, error) {
+
+	p := &PubSub{}
+
+	opts := MQTT.NewClientOptions().AddBroker("tcp://127.0.0.1:1883")
+	opts.SetClientID(nameClient)
+	opts.SetDefaultPublishHandler(f)
+	p.Conn =  MQTT.NewClient(opts)
+	token := p.Conn.Connect();
+	ok := token.WaitTimeout(30 * time.Second)
+	switch {
+	case !ok:
+		return nil, errors.New("Timeout Error at the beginning of the connection")
+	case token.Error() != nil:
+		return nil, token.Error()
+	}
+
+	p.Err = make(chan error)
+
+	return p, nil
+}
+
+//Publish should be executed with a Go routine. The channel obtains the content that must be sent to the local broker.
+func (p *PubSub) Publish(topic string, ch <-chan string) {
+
+	if p.Conn == nil {
+		p.Err <- errors.New("Nil Connection Error, execute NewConnection()")
+		return
+	}
+
+        for v:= range ch {
+		timeStamp := float64(time.Now().UnixNano())/1000000000
+
+		msg := fmt.Sprintf("{\"timeStamp\": %f, \"value\": %q, \"type\": \"GPRMC\"}",timeStamp, v)
+		token := p.Conn.Publish(topic, 0, false, msg)
+		if succ := token.WaitTimeout(10 * time.Second); !succ {
+			p.Err <- errors.New("timeout Error in publish")
+		}
+		log.Printf("message: %s\n",msg)
+	}
+}
+
+//Disconnect and close error channel
+func (p *PubSub) Disconnect() {
+	close(p.Err)
+	p.Conn.Disconnect(250)
+}
