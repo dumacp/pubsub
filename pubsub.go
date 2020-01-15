9742ec308ffe2ad1b5a9c57304216469c035a605
Camilo Zapata <camilo.zapata@nebulae.com.co>
2020-01-14 21:25:12 -0500

    add topic GPS

--- pubsub.go
+++ pubsub.go
@@ -29,8 +29,15 @@ var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
 	fmt.Printf("MSG: %s\n", msg.Payload())
 }
 
+var subcriptions = make([]string, 0)
 var onConnection MQTT.OnConnectHandler = func(c MQTT.Client) {
 	log.Println("OnConnection MQTT")
+	for _, v := range subcriptions {
+		t := c.Subscribe(v, 0, nil)
+		if t.WaitTimeout(3*time.Second) && t.Error() != nil {
+			log.Printf("error subzcription: %s", t.Error())
+		}
+	}
 }
 
 //NewConnection return the PubSub object. nameClient is the client name in local broker.
@@ -56,6 +63,39 @@ func NewConnection(nameClient string) (*PubSub, error) {
 	p.Err = make(chan error)
 
 	return p, nil
+}
+
+//New return the PubSub object. Without start connection, nameClient is the client name in local broker.
+func New(nameClient string) (*PubSub, error) {
+
+	p := &PubSub{}
+
+	opts := MQTT.NewClientOptions().AddBroker("tcp://127.0.0.1:1883")
+	opts.SetClientID(nameClient)
+	opts.SetDefaultPublishHandler(f)
+	opts.SetOnConnectHandler(onConnection)
+	opts.SetAutoReconnect(true)
+	p.Conn = MQTT.NewClient(opts)
+
+//Start connection
+func (p *PubSub) Start() (error) {
+	token := p.Conn.Connect()
+	ok := token.WaitTimeout(30 * time.Second)
+	switch {
+	case !ok:
+		return nil, errors.New("Timeout Error at the beginning of the connection")
+	case token.Error() != nil:
+		return nil, token.Error()
+	}
+
+	p.Err = make(chan error)
+
+	return p, nil
+}
+
+//AddSubscription add new subcription before Connection
+func (p *PubSub) AddSubscription(topic string) {
+	subcriptions = append(subcriptions, topic)
 }
 
 //Publish should be executed with a Go routine. The channel obtains the content that must be sent to the local broker.
